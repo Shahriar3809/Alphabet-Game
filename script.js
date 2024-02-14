3261a1103c7d85a535a1b425dc93102d3b621aac
Shahriar <shahriar3809@gmail.com>
2024-02-14 20:57:55 +0600

    Final

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function showElementById(elementId) {
 function getARandomAlphabet() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabet = alphabetString.split('');
-    const randomNumber = Math.floor(Math.random() * 25 + 1);
+    const randomNumber = Math.round(Math.random() * 25);
     return alphabet[randomNumber];
 }
 
@@ -29,8 +29,73 @@ function addBgColorById(elementId) {
     element.classList.add('text-white');
 }
 
+
+// সঠিক কী প্রেস করে ফেললে ব্যাকগ্রাউন্ড সরিয়ে ফেলা
+function removeBgColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+    element.classList.remove('text-white');
+}
+
+
+function getTextValue(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValue = element.innerText;
+    const value = parseInt(elementValue);
+    return value
+}
+
+function setTextValue(elementId, newSet) {
+    const element = document.getElementById(elementId);
+    element.innerText = newSet;
+}
+
+
+
+
+
 //-------------------Common Function Ends-------------------------
 
+
+
+
+
+
+document.addEventListener('keyup', function (event) {
+    const playerPressed = event.key;
+    
+    const currentAlpha = document.getElementById('current-alphabet').innerText;
+    const expected = currentAlpha.toLowerCase();
+    
+
+    if(playerPressed === expected) {
+        const textValue =  getTextValue('scorePoint');
+        const newScore = textValue + 1;
+        setTextValue('scorePoint', newScore);
+
+        continueGame();
+
+        removeBgColorById(expected);
+     
+    } else {
+        const textValue = getTextValue('lifePoint');
+        const newLife = textValue-1;
+        setTextValue('lifePoint', newLife);
+        if(newLife === -1) {
+            gameOver();
+           
+        }
+        
+    }
+})
+
+
+function gameOver() {
+        hideElementById('play-ground');
+        showElementById('finalScore');
+        const lastScore = getTextValue('scorePoint');
+        setTextValue("finalScoreCard", lastScore);
+}
 
 
 
@@ -42,8 +107,16 @@ function continueGame() {
     addBgColorById(alphabet);
 }
 
+
+
 function play() {
+    const currentAlpha = document.getElementById('current-alphabet').innerText;
+    const expected = currentAlpha.toLowerCase();
+    removeBgColorById(expected);
+    setTextValue('lifePoint', 5);
+    setTextValue('scorePoint', 0);
     hideElementById('home');
+    hideElementById('finalScore');
     showElementById('play-ground');
     continueGame();
 }
